d902e4c5e3431843ba315a2f12c4ba906f3a3876
João Victor Toniazzo de Oliveira <91996228+ToniCross@users.noreply.github.com>
2023-07-03 20:27:14 -0300

    refatoração geral

--- app/pages/cadastro/cadastro.js
+++ app/pages/cadastro/cadastro.js
@@ -1,27 +1,69 @@
-document.getElementById("register-form").addEventListener("submit", function(event) {
+let nameInput = document.getElementById("name");
+let emailInput = document.getElementById("email");
+let telefoneInput = document.getElementById("phone");
+let senhaInput = document.getElementById("password");
+
+nameInput.addEventListener("keydown", function(event) {
+
+  // Eventos de teclado, impressão dos unicodes de cada tecla:
+  console.log("Código da tecla pressionada:", event.key || event.code);
+  if (event.key === "Enter") {
+    event.preventDefault();
+    emailInput.focus();
+  }
+});
+
+emailInput.addEventListener("keydown", function(event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    telefoneInput.focus();
+  }
+});
+
+telefoneInput.addEventListener("keydown", function(event) {
+  if (event.key === "Enter") {
+    event.preventDefault();
+    senhaInput.focus();
+  }
+});
+
+// Eventos de formulário: onfocus e onblur
+nameInput.addEventListener("onblur", function() {
+  nameInput.style.backgroundColor = "black";
+});
+
+nameInput.addEventListener("focus", function() {
+  nameInput.style.backgroundColor = "#693708";
+});
+
+// Criação do usuário e utilizando o local storage, transformando o objeto em um arquivo JSON:
+
+function iniciar() {
+  document.getElementById("register-form").addEventListener("submit", function(event) {
     event.preventDefault(); 
     
-    let name = document.getElementById("name").value;
-    let email = document.getElementById("email").value;
-    let telefone = document.getElementById("phone").value;
-    let senha = document.getElementById("password").value;
+    let name = nameInput.value;
+    let email = emailInput.value;
+    let telefone = telefoneInput.value;
+    let senha = senhaInput.value;
     
     if (name && email && telefone && senha) {
-
-        let userData = {
-            name: name,
-            email: email,
-            telefone: telefone,
-            senha: senha
-          };
+  
+      let userData = {
+          name: name,
+          email: email,
+          telefone: telefone,
+          senha: senha
+        };
       
-          localStorage.setItem("userData", JSON.stringify(userData));
-
-      alertify.alert("Cadastro realizado com sucesso!");
-
+      localStorage.setItem("userData", JSON.stringify(userData));
+  
+      alert("Cadastro realizado com sucesso!");
+  
       window.location.href = "../login/login.html";
     } else {
-
+  
       alert("Por favor, preencha todos os campos!");
     }
   });
+}
--- app/pages/login/login.js
+++ app/pages/login/login.js
@@ -1,32 +1,117 @@
-'use strict';
+// Essa parte é sobre os requisitos da disciplina:
 
-import { LOGGED_IN_KEY, USERNAME_KEY } from '../../util/shared.js';
+let fraseTimeOut3 = "TimeOut de 3 segundos";
+let fraseTimeOut6 = "TimeOut de 6 segundos";
+let fraseTimeOut9 = "TimeOut de 9 segundos";
 
-document.getElementById("loginForm").addEventListener("submit", function(event) {
-  event.preventDefault();
+// Função anônima com argumento:
+setTimeout(function(frase) {
+  console.log(frase);
+}, 3000, fraseTimeOut3);
 
-  let email = document.getElementById("email").value;
-  let senha = document.getElementById("password").value;
+// Função anônima sem argumento:
+setTimeout(function() {
+  console.log(fraseTimeOut6);
+}, 6000);
 
-  console.log("Ele pegou as variáveis");
+setTimeout(function() {
+  exibirMensagem(fraseTimeOut9);
+}, 9000);
 
-  if (email && senha) {
-    let storedData = localStorage.getItem("userData");
-    if (storedData) {
-      let userData = JSON.parse(storedData);
-      if (userData.email === email && userData.senha === senha) {
-        
-        alertify.success("Login realizado com sucesso!");
+// Função com nome:
+function exibirMensagem(frase) {
+  console.log(frase);
+}
 
-        window.location.href = "../produtos/produtos.html";
-        console.log("Era pra ter desviado para a página produtos");
+// Função aninhada e auto executável com arrow function:
+function calculadora() {
+  let a = 6;
+  let b = 2;
+
+  ((x, y) => console.log("A soma de 6 e 2 é ", x + y))(a, b);
+
+  ((x, y) => console.log("A subtração de 6 e 2 é ", x - y))(a, b);
+
+  ((x, y) => console.log("A divisão de 6 e 2 é ", x / y))(a, b);
+}
+
+setTimeout(calculadora, 10000);
+
+// Eventos de movimento de mouse:
+
+let btnLogin = document.getElementById("btnLogin");
+
+btnLogin.addEventListener("mousemove", function() {
+  btnLogin.style.width = "450px";
+  btnLogin.style.height = "60px";
+});
+
+btnLogin.addEventListener("mouseleave", function() {
+  btnLogin.style.width = "395px";
+  btnLogin.style.height = "40px";
+});
+
+// A partir da função iniciar(), o código está voltado para o projeto:
+
+function iniciar() {
+  let emailInput = document.getElementById("email");
+  let senhaInput = document.getElementById("password");
+
+  // Evento de teclado:
+  emailInput.addEventListener("keydown", function(event) {
+    if (event.key === "Enter") {
+      event.preventDefault();
+      senhaInput.focus();
+    }
+  });
+
+  // Validação dos campos e-mail e senha:
+  document.getElementById("loginForm").addEventListener("submit", function(event) {
+    event.preventDefault();
+    let email = emailInput.value;
+    let senha = senhaInput.value;
+
+    if (email && senha) {
+      let storedData = localStorage.getItem("userData");
+      if (storedData) {
+        let userData = JSON.parse(storedData);
+        if (userData.email === email && userData.senha === senha) {
+          
+          // Função sendo passada como argumento para outra função:
+          function saudacao(emailDoUsuario) {
+            alert("Seja bem-vindo usuário do e-mail: " + emailDoUsuario);
+          }
+
+          function realizarSaudacao(funcao, emailDoUsuario) {
+            funcao(emailDoUsuario);
+          }
+
+          realizarSaudacao(saudacao, email);
+
+          // Exemplo das caixas de diálogo
+
+          let pergunta = confirm("Você está feliz?");
+
+          if (pergunta) {
+            alertify.alert("Que bom! Aproveite o nosso site!");
+            window.location.href = "../produtos/produtos.html";
+          } else {
+            alertify.alert("Que pena! Aproveite o nosso site!");
+            window.location.href = "../produtos/produtos.html";
+          }
+        } else {
+          alertify.error("E-mail ou senha incorretos. Por favor, tente novamente.");
+        }
       } else {
-        alertify.error("Email ou senha incorretos. Por favor, tente novamente.");
+        alertify.alert("Não há registro de usuário. Por favor, cadastre-se primeiro.");
       }
     } else {
-      alertify.alert("Não há registro de usuário. Por favor, cadastre-se primeiro.");
+      alertify.alert("Por favor, preencha todos os campos!");
     }
-  } else {
-    alertify.alert("Por favor, preencha todos os campos!");
-  }
+  });
+
+}
+
+document.addEventListener("DOMContentLoaded", function() {
+  iniciar();
 });
